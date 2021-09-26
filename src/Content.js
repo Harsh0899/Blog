54dfc79ae32e6a36b71ead1fa12356dc2e1c9122
Harsh <harshy080201@gmail.com>
2021-09-26 14:52:42 +0530

    Initial commit

---
+++ src/Content.js
@@ -0,0 +1,24 @@
+const Content=()=>{
+    return(
+        <div className="container">
+            <h1>Hi! This is an Online Journal Maintained by Fr0ztyy</h1>
+            <p className="d1 desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptatum molestias repudiandae, dolores quas sequi explicabo veniam dolorum pariatur, 
+            illum esse quisquam vitae id, dicta placeat quos? Aspernatur maiores consequatur doloremque.</p>
+            <p className="d2 desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptatum molestias repudiandae, dolores quas sequi explicabo veniam dolorum pariatur, 
+            illum esse quisquam vitae id, dicta placeat quos? Aspernatur maiores consequatur doloremque.</p>
+            <p className="d3 desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptatum molestias repudiandae, dolores quas sequi explicabo veniam dolorum pariatur, 
+            illum esse quisquam vitae id, dicta placeat quos? Aspernatur maiores consequatur doloremque.</p>
+            <p className="d3 desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptatum molestias repudiandae, dolores quas sequi explicabo veniam dolorum pariatur, 
+            illum esse quisquam vitae id, dicta placeat quos? Aspernatur maiores consequatur doloremque.</p>
+            <p className="d4 desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptatum molestias repudiandae, dolores quas sequi explicabo veniam dolorum pariatur, 
+            illum esse quisquam vitae id, dicta placeat quos? Aspernatur maiores consequatur doloremque.</p>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar=()=>{
+    return(
+        <div className="nav">
+            <h1>Fr0ztyy's Journal</h1>
+            <div className="links">
+                <a href="#">Home</a>
+                <a href="#" className="newblog">New Entry</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
